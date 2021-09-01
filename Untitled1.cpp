69f256dadae2b58010723bff98fe2f07239b1b47
Pragyan Khati <83399260+rampdedlizer@users.noreply.github.com>
2021-09-01 19:47:13 +0545

    InitialIn

---
+++ Untitled1.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+
+class staff
+{
+	int code;
+	std::string name;
+public:
+	void input()
+	{
+		std::cout<<""<<std::endl;
+		std::cout << "Enter your code : ";
+		std::cin >> code;
+
+		std::cout << "Enter your name : ";
+		std::cin >> name;
+	}
+	void display()
+	{
+		std::cout << "Your code number is " << code << std::endl;
+		std::cout << "Your name is " << name << std::endl;
+	}
+};
+
+class teacher :public staff
+{
+protected:
+	std::string subject;
+	std::string publication;
+public:
+	void in1()
+	{
+		input();
+		std::cout << "Enter the subject you are teaching : ";
+		std::cin >> subject;
+
+		std::cout << "Enter the publication you are using : ";
+		std::cin >> publication;
+	}
+
+	void d1()
+	{
+		
+		display();
+
+		std::cout << "The subject you are teaching is " << subject << std::endl;
+		std::cout << "The publication that you are using is " << publication << std::endl;
+
+		std::cout << "" << std::endl;
+	}
+};
+
+class officer :public staff
+{
+protected:
+	int grade;
+public:
+	void enter()
+	{
+		input();
+		std::cout << "Enter your grade : ";
+		std::cin >> grade;
+	}
+	
+	void out()
+	{
+		
+		display();
+		std::cout << "Your grade is " << grade << std::endl;
+
+		std::cout << "" << std::endl;
+	}
+
+};
+
+class typist :public staff
+{
+protected:
+	int speed;
+public:
+
+};
+
+class casual :public typist
+{
+	int dw;
+public:
+	void speak()
+	{
+		input();
+
+		std::cout << "Enter your daily wages : ";
+		std::cin >> dw;
+
+		std::cout << "Enter your speed : ";
+		std::cin >> speed;
+	}
+	void spread()
+	{
+		
+		display();
+		
+		std::cout << "Your speed is " << speed << std::endl;
+		std::cout << "Your daily wages is " << dw << std::endl;
+	}
+};
+
+int main()
+{
+	// Teacher
+	teacher	a1;
+	a1.in1();
+	//a1.d1();
+
+	std::cout << "" << std::endl;
+	
+	//Officer
+	officer a2;
+	a2.enter();
+	//a2.out();
+
+	//Casual
+	casual a3;
+	a3.speak();
+	//a3.spread();
+
+	int z;
+
+	std::cout << "Enter whether you want to access teachers' credentials : ";
+	std::cin >> z;
+
+	if (z == 0)
+	{
+		a1.d1();
+	}
+	else if(z == 1)
+	{
+		a2.out();
+	}
+	else{ a3.spread(); }
+		
+}
